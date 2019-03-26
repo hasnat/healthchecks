e6026e85282fd70d348a8aa837b1a6aed055ebee
Hasnat <hasnat.ullah@gmail.com>
2019-03-26 19:23:46 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,11 @@
 
 ./manage.py migrate
 
-(./manage.py sendalerts > /dev/stdout 2>&1 &) && ./manage.py runserver 0.0.0.0:8000
+# run notification alerts (use while to mock as supervisor)
+(while sleep 3
+do
+echo "(re)starting alerts"
+./manage.py sendalerts > /dev/stdout
+done) 2>&1 &
 
+./manage.py runserver 0.0.0.0:8000
